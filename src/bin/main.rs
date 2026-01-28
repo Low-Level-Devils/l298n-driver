12c3b4a855a872e37d039366ae7060b5d41f22e5
Josh Walker <walker11710@gmail.com>
2026-01-27 17:19:42 -0700

    add single motor test. Requires testing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,9 +8,14 @@
 #![deny(clippy::large_stack_frames)]
 
 use esp_hal::clock::CpuClock;
-use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
+use esp_hal::ledc::channel::ChannelIFace;
+use esp_hal::ledc::timer::TimerIFace;
+use esp_hal::{main, peripherals};
+use esp_hal::time::{Duration, Instant, Rate};
 use log::info;
+use esp_hal::ledc::*;
+use esp_hal::gpio::*;
+use esp_hal::delay::Delay;
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -32,12 +37,71 @@ fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
+
+    //let output_config = OutputConfig::default();
+    //let mut motor_anode = Output::new(peripherals.GPIO2, Level::Low, output_config);
+    //let mut motor_cathode = Output::new(peripherals.GPIO4, Level::Low, output_config);
+
+    let mut delay = Delay::new();
+
+    let mut ledc = Ledc::new(peripherals.LEDC);
+    ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
+
+    let mut lstimer0 = ledc.timer::<LowSpeed>(timer::Number::Timer0);
+    lstimer0.configure(timer::config::Config {
+        duty: timer::config::Duty::Duty5Bit,
+        clock_source: timer::LSClockSource::APBClk,
+        frequency: Rate::from_hz(24),
+    }).expect("Error configuring lstimer");
+
+    let mut motor_anode_ledc_channel = ledc.channel(channel::Number::Channel0, peripherals.GPIO2);
+    motor_anode_ledc_channel.configure(channel::config::Config {
+        timer: &lstimer0,
+        duty_pct: 0,
+        drive_mode: DriveMode::PushPull
+    }).expect("Fail to configure motor anode channel");
+
+    let mut motor_cathode_ledc_channel = ledc.channel(channel::Number::Channel0, peripherals.GPIO4);
+    motor_cathode_ledc_channel.configure(channel::config::Config {
+        timer: &lstimer0,
+        duty_pct: 0,
+        drive_mode: DriveMode::PushPull
+    }).expect("Fail to configure motor cathode channel");
 
     loop {
-        info!("Hello world!");
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        info!("Motor Forward Full");
+        motor_anode_ledc_channel.set_duty(100).expect("Failed to set duty anode 1");
+        motor_cathode_ledc_channel.set_duty(0).expect("Failed to set duty cathode 1");
+        delay.delay_millis(2000);
+        info!("Motor reverse Full");
+        motor_anode_ledc_channel.set_duty(0).expect("Failed to set duty anode 2");
+        motor_cathode_ledc_channel.set_duty(100).expect("Failed to set duty cathode 2");
+        delay.delay_millis(2000);
+        info!("Motor Forward 25%");
+        motor_anode_ledc_channel.set_duty(25).expect("Failed to set duty anode 3");
+        motor_cathode_ledc_channel.set_duty(0).expect("Failed to set duty cathode 3");
+        delay.delay_millis(2000);
+        info!("Motor Forward 50%");
+        motor_anode_ledc_channel.set_duty(50).expect("Failed to set duty anode 4");
+        motor_cathode_ledc_channel.set_duty(0).expect("Failed to set duty cathode 4");
+        delay.delay_millis(2000);
+        info!("Motor Forward 75%");
+        motor_anode_ledc_channel.set_duty(75).expect("Failed to set duty anode 5");
+        motor_cathode_ledc_channel.set_duty(0).expect("Failed to set duty cathode 5");
+        delay.delay_millis(2000);
+        info!("Motor Reverse 25%");
+        motor_anode_ledc_channel.set_duty(0).expect("Failed to set duty anode 6");
+        motor_cathode_ledc_channel.set_duty(25).expect("Failed to set duty cathode 6");
+        delay.delay_millis(2000);
+        info!("Motor Reverse 50%");
+        motor_anode_ledc_channel.set_duty(0).expect("Failed to set duty anode 7");
+        motor_cathode_ledc_channel.set_duty(50).expect("Failed to set duty cathode 7");
+        delay.delay_millis(2000);
+        info!("Motor Reverse 75%");
+        motor_anode_ledc_channel.set_duty(0).expect("Failed to set duty anode 8");
+        motor_cathode_ledc_channel.set_duty(75).expect("Failed to set duty cathode 8");
+        delay.delay_millis(2000);
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
